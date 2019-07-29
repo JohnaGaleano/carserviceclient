ade6a7d0ebd260128a234e5d21e8e411b376d4f9
johnagaleano <johnagaleano@gmail.com>
2019-07-29 13:51:34 -0500

    car with owner list

--- src/app/car-owner-list/car-owner-list.component.ts
+++ src/app/car-owner-list/car-owner-list.component.ts
@@ -13,37 +13,36 @@ import { CarWithOwner } from '../models/carAndowner.model';
 export class CarOwnerListComponent implements OnInit {
   public cars = [];
   public owners: Array<any>;
-  public carWithOwner: CarWithOwner;
-  public list: CarWithOwner[] = [];
-
+  public list: Array<any>;
 
   constructor(private carService: CarService, private ownerService: OwnerService, private giphyService: GiphyService) { }
 
   ngOnInit() {
+    this.list = [];
     this.ownerService.getAll().subscribe((result: any) => {
       this.owners = result._embedded.owners;
-    });
-    this.carService.getAll().subscribe(data => {
-      for (const car of data) {
-        if (car.ownerDni) {
-          this.carWithOwner = car;
-          console.log(this.carWithOwner);
-
-          this.giphyService.get(this.carWithOwner.name)
-            .subscribe(url => {
-              this.carWithOwner.giphyUrl = url;
-              for (const owner of this.owners) {
-                if (this.carWithOwner.ownerDni === owner.dni) {
-                  this.carWithOwner.profession = owner.profession;
-                  this.carWithOwner.ownerName = owner.name;
+      this.carService.getAll().subscribe(data => {
+        for (const car of data) {
+          if (car.ownerDni) {
+            this.giphyService.get(car.name)
+              .subscribe(url => {
+                car.giphyUrl = url;
+                for (const owner of this.owners) {
+                  if (car.ownerDni === owner.dni) {
+                    this.list.push({
+                      carName: car.name,
+                      ownerDni: car.ownerDni,
+                      ownerName: owner.name,
+                      profession: owner.profession,
+                      giphyUrl: car.giphyUrl
+                    });
+                  }
                 }
-              }
-            });
-          this.list.push(this.carWithOwner);
+              });
+          }
         }
-      }
+      });
     });
 
   }
-
 }
